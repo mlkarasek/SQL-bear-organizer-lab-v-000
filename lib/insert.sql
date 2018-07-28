08a4430f1a1bc8f2320116a6e7b446296d16e00a
Monica  Karasek <mlkarasek@gmail.com>
2018-07-28 01:54:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr. Chocolate" 20, "M", "dark brown", "calm", 0);
+VALUES ('Mr. Chocolate', 15, 'M', 'brown', 'calm', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Rowdy" 10, "M", "black", "intense", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
